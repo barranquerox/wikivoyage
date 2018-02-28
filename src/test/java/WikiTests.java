1db31fff31fd028c806083bb03afbb80bd824bfb
Luis Garelli <luiscarlosgarelli@gmail.com>
2018-02-28 11:54:53 +0100

    added more tests

--- src/test/java/WikiTests.java
+++ src/test/java/WikiTests.java
@@ -20,23 +20,40 @@ public class WikiTests {
     public void test1()
     {
         MainPage mainPage = new MainPage(driver);
-        mainPage.search("Paris")
-                .header.search("Lyon")
-                .navigationPanel.goToHomePage()
-                .header.search("Venezuela")
-                .navigationPanel.goToHelp();
+        mainPage.search("Paris");
     }
 
     @Test
     public void test2()
     {
-
+        MainPage mainPage = new MainPage(driver);
+        String recherche = System.getProperty("recherche");
+        mainPage.search(recherche);
     }
 
     @Test
     public void test3()
     {
+        MainPage mainPage = new MainPage(driver);
+        DestinationPage destination = mainPage.search("Paris");
+        HomePage home = destination.navigationPanel.goToHomePage();
+    }
 
+    @Test
+    public void test4()
+    {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.search("Paris").navigationPanel.goToHomePage();
+    }
+
+    @Test
+    public void test5()
+    {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.search("Paris")
+                .header.search("Lyon")
+                .header.search("Madrid")
+                .navigationPanel.goToHomePage();
     }
 
     @After
